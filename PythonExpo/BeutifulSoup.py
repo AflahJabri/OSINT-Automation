b27e3b3e1fd6dd759aff4f9d295aa9732bfd96dc
AflahJabri <85798819+AflahJabri@users.noreply.github.com>
2024-04-02 12:02:35 +0200

    Add files via upload

---
+++ PythonExpo/BeutifulSoup.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Make a request to the website
+url = 'https://www.kompass.com/z/nl/r/north-brabant/nl_30/'
+response = requests.get(url)
+
+# Parse the HTML content
+soup = BeautifulSoup(response.content, 'html.parser')
+
+
+organization_names = soup.select('div.company-container')
+
+for org in organization_names:
+    # Assuming the organization name is in a <span> or <a> tag within the div
+    title = org.find('title') or org.find('a')
+    if title:
+        print(title.text.strip())
+
+
+
+# Navigate to the targetted HTML container
+# body = soup.find('body', class_='classificationListCompany')
+# dive deeper 
+# and deeper
+# main_div = soup.find('div', class_='main-div')
+
+
+
+
+# Extract organization names from div in the homepage
+#organization_names = []
+#for div in soup.find_all('div', class_='col col-title company-container'):
+    #organization_names.append(div.text.strip())
+
+#target_data = soup.select_one('body.classificationListCompany main.main div.container div.nomenclature_wrapper div.resultatsListe div.container div.row section.resultants div.row div.prod_list div.company-container').text.strip()
---
+++ PythonExpo/soup2.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+
+def scrapecompany_names(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    num_companies_str = soup.find('span', class_='btn-filter filterEnterprise').text.strip().split()[0]
+    numcompanies = int(num_companies_str.replace(',', ''))
+
+    companies_per_page = len(soup.find_all('div', class_='product-list-item'))
+
+    companynames = []
+    for page_num in range(1, num_companies // companies_per_page + 1):
+        params = {'page': page_num}
+        response = requests.get(url, params=params)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        companies = soup.find_all('div', class_='product-list-item')
+        for company in companies:
+            name = company.find('h3', class_='product-title').text.strip()
+            company_names.append(name)
+    return company_names
+
+url = 'https://www.kompass.com/z/nl/r/north-brabant/nl_30/'
+company_names = scrapecompany_names(url)
+
+for name in company_names:
+    print(name)
