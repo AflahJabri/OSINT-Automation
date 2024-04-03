15048801159c1f4bf550ab52d2566906a4274482
AflahJabri <aflah.aljabri@hotmail.com>
2024-04-03 15:21:02 +0200

    edits

--- PythonExpo/companyScraping.py
+++ PythonExpo/companyScraping.py
@@ -15,7 +15,7 @@ options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option("useAutomationExtension", False) 
 
 
-
+# Initiate driver/browser with the set options above
 browser = webdriver.Chrome(options=options) 
 browser.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})") 
 
@@ -36,13 +36,19 @@ listings = browser.find_elements(By.CLASS_NAME, "product-list-data")
 for li in listings:
     # Search for name of company via link tags <a> 
     findLink = li.find_element(By.TAG_NAME, "a")
+    # click on link and go the company specific page
+    findLink.click()
+    findkvk = browser.find_element(By.XPATH, "//th[text()='KvK nummer']/following-sibling::td")
+    kvkNumber = findkvk.text
+    browser.back()
     try:
         # Search for <div> containing company URL
         findWebsite = li.find_element(By.CLASS_NAME, "companyWeb")
         url = findWebsite.find_element(By.TAG_NAME, "a")
-        print("Comapany: " + findLink.text, "  URL: " + url.get_attribute('href'))  
+        print("Comapany: " + findLink.text,"with KvK number: " + kvkNumber + " with URL: " + url.get_attribute('href'))
+    # An expection to handle crashes incase a comany has no website  
     except NoSuchElementException:
-        print("Company: " + findLink.text + " has no website.")
+        print("Company: " + findLink.text + "with KvK number: " + kvkNumber + " but has no website.")
 
 
 ## Once done with the loop try clicking on next page, wait for 5 seconds and repeat loop
