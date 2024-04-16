3060469231dcbd109263e170536632de91076329
AflahJabri <aflah.aljabri@hotmail.com>
2024-04-16 15:54:41 +0200

    Update companyScraping.py

--- PythonExpo/companyScraping.py
+++ PythonExpo/companyScraping.py
@@ -30,29 +30,38 @@ listElement = WebDriverWait(browser, 5).until(
 
 # Searching for individual company listings
 listings = browser.find_elements(By.CLASS_NAME, "product-list-data")
-
+baseURL = "https://www.kompass.com/z/nl/r/north-brabant/nl_30/"
 # Loop through each listing
 for li in listings:
     # Search for name of company via link tags <a> 
     findLink = li.find_element(By.TAG_NAME, "a")
     # Click on link and go the company specific page
-    hyperLink = findLink.get_attribute('href')
-    browser.execute_script(f"window.open('{hyperLink}', '_blank');")
-    newTab = browser.window_handles[-1]
-    browser.switch_to.window(newTab)
+    findDiv = li.find_element(By.CLASS_NAME, 'row rowTop')
+    findNextPage = findDiv.find_element(By.TAG_NAME, "a")
+    findNextPage.click()
+    
+    # dynamicURL = findLink.get_attribute('href')
+    # fullURL = baseURL + dynamicURL
+    
+    # print("link : " + fullURL)
+    # browser.execute_script(f"window.open(arguments[0], '_blank');", fullURL)
+    # newTab = browser.window_handles[-1]
+    # browser.switch_to.window(newTab)
     # Once in the next page search for table row with KvK number
-    findkvk = browser.find_element(By.XPATH, "//th[text()='KvK nummer']/following-sibling::td")
-    kvkNumber = findkvk.text
+    # findkvk = browser.find_element(By.XPATH, "//th[text()='KvK nummer']/following-sibling::td")
+    # kvkNumber = findkvk.text
     # Back to previous page
+    time.sleep(3)
     browser.close()
+    time.sleep(2)
     try:
         # Search for <div> containing company URL
         findWebsite = li.find_element(By.CLASS_NAME, "companyWeb")
         url = findWebsite.find_element(By.TAG_NAME, "a")
-        print("Comapany: " + findLink.text,"with KvK number: " + kvkNumber + " with URL: " + url.get_attribute('href'))
+        print("Comapany: " + findLink.text,"with KvK number: " + 'kvkNumber' + " with URL: " + url.get_attribute('href'))
     # An expection to handle crashes incase a comany has no website  
     except NoSuchElementException:
-        print("Company: " + findLink.text + "with KvK number: " + kvkNumber + " but has no website.")
+        print("Company: " + findLink.text + "with KvK number: " + 'kvkNumber' + " but has no website.")
 
 
 ## Once done with the loop try clicking on next page, wait for 5 seconds and repeat loop
@@ -65,3 +74,4 @@ browser.quit()
 # //*[@id="seoProdListNLN0672591"]/div/div[2]/div
 # /html/body/main/div/div[2]/div/div/div[2]/section[2]/div[3]/div[1]/div/div[2]/div/a
 # /html/body/main/div/div[2]/div/div/div[2]/section[2]/div[3]/div[1]/div
+
